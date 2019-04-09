1252c53424d077bca2741a27947ee0be00e871c5
anees mangi <mangianees@gmail.com>
2019-04-09 20:11:00 +0100

    manager resume

---
+++ index.php
@@ -0,0 +1,331 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Arima+Madurai" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="shortcut icon" href="./img/profile1.ico" />
+
+
+    <title>Anees Mangi (Manager)</title>
+</head>
+
+<body>
+
+<div id="navbar">
+  <a href="#summary">Summary</a>
+  <a href="#core">Core Competencies</a>
+  <a href="#experience">Experience</a>
+  <a href="#education">Education</a>
+  <a href="#education">Skills</a>
+  <a href="#education">Trainings</a>
+  <a href="#education">Co-curricular Activities</a>
+  <a href="#education">Reference</a>
+</div>
+
+
+<!-- social media bar -->
+
+
+<div class="icon-bar">
+  <a href="https://www.facebook.com/aneesmangichak/" target="_blank"><i class="fa fa-facebook"></i></a> 
+  <a href="https://twitter.com/mangianees" target="_blank"><i class="fa fa-twitter"></i></a> 
+  <a href="https://www.linkedin.com/in/anees-mangi-60947118/" target="_blank"><i class="fa fa-linkedin"></i></a>
+  
+</div>
+<section id="summary"> </section>
+
+<div class="content">
+  <h3>Sticky Social Bar</h3>
+</div>
+
+
+               
+        <div class="bg-text">
+            <div class="headingss">
+            
+                <h1>Anees Mangi</h1>
+                <p>M A N A G E R / I T  -  E X P E R T</p>
+                
+                <div class="row">
+                    <div class="col">
+                    <p class="fas fa-map-marker-alt"> <div class="headingss"> Birmingham </div></p> 
+                    </div>
+                    <div class="col">
+                    <p class="fas fa-mobile"> <div class="headingss"> 07443986452 </div></p>
+                    </div>
+                    <div class="col">
+                    <p class="fas fa-at"> <div class="headingss">mangianeesbirmingham@gmail.com</div></p>
+                    </div>
+                </div>    
+            </div>
+
+            
+                <div style="border:0.3pt solid white; width:100%; height:0.01 cm">
+                <div class="container text-style">
+                    <br>
+                           
+                    <h6>Summary</h6>
+                    <ul>
+                    <li>Equipped with a strategic mindset that facilitates the formulation and implementation of
+                        strategies that have significantly improved preventative maintenance and cost
+                        efficiency.
+                    </li>
+                    <li>Practiced in project and construction management including initial planning and
+                        scoping, scheduling, sourcing and procurement of materials and testing of integrated
+                        utility management systems.
+                    </li>
+                    <section id="core"></section>
+                    <li>Recognized as a builder and leader of high performance teams by engendering a
+                        culture of trust and collaboration and valuing ongoing training and development to build
+                        skills and professionalism.
+                    </li>
+                    <li>Exemplary interpersonal, leadership, communication, and presentation skills.
+                    </li>
+                    </ul>
+                
+                <br>
+                
+                <div class="container text-style">
+                    <h6>Core Competencies</h6>
+                    <div class="row">
+                        <div class="col">
+                    
+                            <ul>
+                                <li>Facilities Management</li>
+                                <li>Maintenance Management</li>
+                                <li>Preventative Maintenance</li>
+                                <li>Site Management</li>
+                                <li>General Management</li>
+                            </ul>
+
+                        </div>
+
+                        <div class="col">
+                        
+                            <ul>
+                                <li>Team Leadership</li>
+                                <li>Project Management</li>
+                                <li>Budget Management</li>
+                                <li>Strategic Planning & Analysis</li>
+                                <li>Operational Planning</li>
+                            </ul>
+                            
+                        </div>
+
+                        <div class="col">
+                        
+                            <ul>
+                                <li>Process Improvements</li>
+                                <li>Relationship Management</li>
+                                
+                                <li>Regulatory</li>
+                                <li>Compliance/Audits</li>
+                                <section id="experience"></section>
+                                <li>Customer Care</li>
+                            </ul>
+                                
+                        </div>
+                    </div>
+                </div>
+                
+<br>
+
+
+                <div class="container text-style">
+                
+                    <h6>Experience</h6>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <p>2005 – 2018</p>
+                        </div>
+                        <div class="col-sm-8">
+                            <h6>Manager (National Database and Registration Authority)</h6>
+                            <h6>Pakistan Consulate Birmingham (Government Organization)</h6>
+                        </div>
+<br>                        
+
+                    <ul>
+                        <li>
+                        <p>Supervised the team of 10 individuals and maintenance mechanics, overseeing activities
+                        and monitoring quality of work. Engendered a strong collaborative work culture, driving
+                        efficiency while managing maintenance and operations. Proficiently managed
+                        budgeting and planning including environmental cleaning, preventative maintenance
+                        and equipment maintenance.</p>
+
+                        </li>
+                        
+                        <li>
+                        <p>Designed, reviewed and updated policies and procedures and established strict
+                        guidelines to ensure that all aspects.</p>
+                    </li>
+
+                    </ul>
+        
+                     </div> <!--row ended -->
+
+                        <div class="row">
+                        <div class="col-sm-4">
+                            <p>2003 – 2005</p>
+                        </div>
+                        <div class="col-sm-8">
+                            <h6>System Engineer (Cosmos Communication Pvt Ltd)</h6>
+                        </div>
+<br>                        
+
+                        <ul>
+                            <li>
+                            <p>Performed detailed analysis of business requirements and developed comprehensive
+                            plans defining and scoping project requirements.</p>
+                            </li>
+                            <li>
+                            <p>Created specific software and devised testing scenarios to ensure optimum levels of
+                            efficiency.</p>    
+                            </li>
+                            <li>
+                            <p>Established clear timeline and monitoring systems to successfully manage within agreedupon
+                            time frame and budget.</p>
+                            </li>
+                        </ul>
+                                        
+    
+</div> <!--row ended -->
+
+</div>
+
+<br>
+                <div class="container text-style">
+                
+                    <h6>Education</h6>
+                <ul>
+                    <li>
+                    <h6>M B A (Management Information Systems)</h6>
+                    </li>
+                    <li>
+                    <h6>Bachelor in Computer Science</h6>
+                    </li>
+                    
+                </ul>
+                <section id="education"></section>
+               </div>
+
+
+<br>
+                <div class="container text-style">
+                    <h6>Skills</h6>
+                <ul>
+                    <li>
+                    Proficient in a variety of maintenance and utility management systems
+                    </li>
+
+                    <li>
+                    Microsoft Office Suite: Word, Excel, Access, Outlook, PowerPoint.
+                    </li>
+                </ul>
+               </div>
+
+
+               <div class="container text-style">
+                    <h6>Trainings</h6>
+                    <div class="row">
+                        <div class="col">
+                            <ul>
+                                <li>
+                                DAU Operations
+                                </li>
+                                <li>
+                                Train the Trainers
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <ul>
+                                <li>
+                                    Operations Policies & Procedures
+                                </li>                 
+                                <li>
+                                    Train the Trainers
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                    
+               </div>
+
+               <div class="container text-style">
+                <h6>Co-curricular Activities</h6>
+
+                <div class="row">
+                    <div class="col">
+                    <ul>
+                        <li>
+                        Software Engineering Compititions                
+                        </li>
+                        <li>
+                        Cricket
+                        </li>
+                        <li>
+                        Music
+                        </li>
+                    </ul>
+                    </div>
+
+                    <div class="col">
+                    <ul>
+                    <li>
+                    Reading Books
+                    </li>
+                    <li>
+                    Movies
+                    </li>
+                    <li>
+                    Current Affairs
+                    </li>
+                    </ul>
+                    </div>
+                </div>
+
+                <div class="container text-style">
+                    <h6>Reference</h6>
+                    <p>Will be furnished on request.</p>
+                </div>  
+
+            </div>
+</div>
+
+
+
+<!-- javascript for navbar -->
+<script>
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+</script>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</body>
+</html>

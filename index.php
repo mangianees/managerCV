85f5459bce171ed1e48a6b3c84174b38bbc7bbc3
anees mangi <mangianees@gmail.com>
2019-04-09 20:21:52 +0100

    SEO added

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Manager Manager IT Store Manager Assistant Manager Depot Manager Management Managerial Anees Mangi">
+     <meta name="author" content="Anees Mangi |Manager | Web Developer | PHP | MYSQL | Python | Django | jquery | wordpress">
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Arima+Madurai" rel="stylesheet">
